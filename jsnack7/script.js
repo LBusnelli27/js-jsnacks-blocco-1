46882e674f837549572941567e468cf3f10d2da4
Busna27 <lucabusnelli2001@gmail.com>
2022-03-01 10:36:04 +0100

    refarctor script jsnack7

--- jsnack7/script.js
+++ jsnack7/script.js
@@ -1,23 +1,23 @@
-const array = [];
+const arrayNumbers = [];
 
 let sum = 0;
 let average = 0;
 
 // Generate random number and push in the array
-for(i = 0; i < 10; i++) {
+for(let i = 0; i < 10; i++) {
     let randomInt = Math.floor(Math.random() * 10) + 1;
-    array.push(randomInt);
+    arrayNumbers.push(randomInt);
 }
 
 // For sum all the number in the array
-for (let i = 0; i < array.length; i++) {
-    sum += array[i];
+for (let i = 0; i < arrayNumbers.length; i++) {
+    sum += arrayNumbers[i];
 }
 
 
 // To make the average number
-average = sum / array.length;
+average = sum / arrayNumbers.length;
 
-console.log(array);
+console.log(arrayNumbers);
 console.log(`This is the sum of array numbers: ${sum}`);
 console.log(`This is the average of array numbers: ${average}`);
