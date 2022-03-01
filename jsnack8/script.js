20c75332d75f71cba425b6f1237b66b1909e5ca9
Busna27 <lucabusnelli2001@gmail.com>
2022-03-01 11:45:54 +0100

    js comments jsnack8

--- jsnack8/script.js
+++ jsnack8/script.js
@@ -2,20 +2,23 @@ const nomi = ["reckless","rectangular","ready","real","realistic","reasonable","
 
 const cognomi = ["people","history","way","art","world","information","map","family","government","health","system","computer","meat","year","thanks","music","person","reading","method","data","food","understanding","theory","law","bird","literature","problem","software","control","knowledge","power","ability","economics","love","internet","television","science","library","nature","fact","product","idea","temperature","investment","area","society","activity","story","magazine","newspaper","relationship","teaching","cell","dealer","debate","finding","lake","member","message","phone","scene","appearance","association","concept","customer","death","discussion","housing","inflation","insurance","mood","woman","advice","blood","effort","expression","importance","opinion","payment","reality","responsibility","situation","skill","statement","wealth","application","city","county","depth"];
 
-
+// Array 10 guests list
 const listaInvitati = [];
 
 
+// Cicle to make 10 randomized firstname + lastname
 for(let i = 0; i < 10; i++) {
+    // Gen position for firstname and lastname
     let randomNome = Math.floor(Math.random() * nomi.length);
     let randomCognome = Math.floor(Math.random() * cognomi.length);
 
+    // Uppercase for first letter of firstname
     const currentNome = nomi[randomNome][0].toUpperCase() + nomi[randomNome].substring(1);
+    // Uppercase for first letter of lastname
     const currentCognome = cognomi[randomCognome][0].toUpperCase() + cognomi[randomCognome].substring(1);
 
-    let currentGuest;
 
-    currentGuest = currentNome + " " + currentCognome;
+    let currentGuest = currentNome + " " + currentCognome;
     listaInvitati.push(currentGuest);
 }
 
