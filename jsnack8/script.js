73926c6ede7dda2455deab6cb0ed22d7fd2a0620
Busna27 <lucabusnelli2001@gmail.com>
2022-03-01 11:41:08 +0100

    jsnack8 - done

---
+++ jsnack8/script.js
@@ -0,0 +1,22 @@
+const nomi = ["reckless","rectangular","ready","real","realistic","reasonable","red","reflecting","regal","regular","reliable","relieved","remarkable","remorseful","remote","repentant","required","respectful","responsible","repulsive","revolving","rewarding","rich","rigid","right","ringed","ripe","roasted","robust","rosy","rotating","rotten","rough","round","rowdy","royal","rubbery","spectavid","voluminous","wan","warlike","warm","warmhearted","warped","wary","wasteful","watchful","waterlogged","watery","wavy","wealthy","weak","weary","webbed","wee","weekly","weepy","weighty","weird","welcome","well-documented","well-groomed","well-informed","well-lit","well-made","well-off","well-to-do","well-worn","wet","which","whimsical","whirlwind","whispered","white","whole","whopping","wicked","wide","wide-eyed","wiggly","wild","willing","wilted","winding","windy","winged","wiry","wise","witty","wobbly","woeful","wonderful","wooden","woozy","wordy","worldly","worn","worried","worrisome","worse","worst","worthless","worthwhile","worthy","wrathful","wretched","writhing","wrong","wry","yawning","yearly","yellow","yellowish","young","youthful","yummy","zany","zealous","zesty","zigzag","rocky"];
+
+const cognomi = ["people","history","way","art","world","information","map","family","government","health","system","computer","meat","year","thanks","music","person","reading","method","data","food","understanding","theory","law","bird","literature","problem","software","control","knowledge","power","ability","economics","love","internet","television","science","library","nature","fact","product","idea","temperature","investment","area","society","activity","story","magazine","newspaper","relationship","teaching","cell","dealer","debate","finding","lake","member","message","phone","scene","appearance","association","concept","customer","death","discussion","housing","inflation","insurance","mood","woman","advice","blood","effort","expression","importance","opinion","payment","reality","responsibility","situation","skill","statement","wealth","application","city","county","depth"];
+
+
+const listaInvitati = [];
+
+
+for(let i = 0; i < 10; i++) {
+    let randomNome = Math.floor(Math.random() * nomi.length);
+    let randomCognome = Math.floor(Math.random() * cognomi.length);
+
+    const currentNome = nomi[randomNome][0].toUpperCase() + nomi[randomNome].substring(1);
+    const currentCognome = cognomi[randomCognome][0].toUpperCase() + cognomi[randomCognome].substring(1);
+
+    let currentGuest;
+
+    currentGuest = currentNome + " " + currentCognome;
+    listaInvitati.push(currentGuest);
+}
+
+console.log(listaInvitati);
